135a5e544d371d33f0b30963600dc25af5f892a1
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-10 22:37:28 +0300

    add SuperHeroAPI

---
+++ SuperHeroAPI/SuperHeroAPI/Migrations/20230610182839_addSuperHeroToDb.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeroAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSuperHeroToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperHeroAPI;
+
+public class SuperHero
+{
+    public int Id { get; set; }
+    
+    public string? Name { get; set; }
+    
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+    
+    public string? Place { get; set; }
+}
